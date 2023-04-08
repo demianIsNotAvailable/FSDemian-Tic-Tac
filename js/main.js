4060eda9027ddccca4debb290b01aad41407c0a3
demianIsNotAvailable <zansky@gmail.com>
2023-04-08 14:03:00 +0200

    Logro desbloqueado: dinamitar repo sin querer. Reglas especiales funcionando

--- js/main.js
+++ js/main.js
@@ -1,26 +1,25 @@
 const jugador1 = "O";
 const jugador2 = "X";
 
-let jugadorActual = jugador1
+let jugadorActual = jugador1;
+let turnos = 0;
 
-
-
-let tableroLleno = false;
 let tablero = ["", "", "", "", "", "", "", "", ""];
 
-const board_container = document.querySelector(".juego");
+const mesa = document.querySelector(".juego");
 
 // funciones
 
 // crear tablero
 
 const crearTablero = () => {
-  board_container.innerHTML = "";
+  mesa.innerHTML = "";
   tablero.forEach((e, i) => {
-    board_container.innerHTML += `<div id="casilla${i}" class="casilla" onclick="mover(${i})">${tablero[i]}</div>`;
-    if (e == jugador1 || e == jugador2) {
+    mesa.innerHTML += `<div id="casilla${i}" class="casilla" onclick="mover(${i})">${tablero[i]}</div>`;
+    if (turnos < 6 && (e == jugador1 || e == jugador2)) {
       document.querySelector(`#casilla${i}`).classList.add("ocupada");
-    }
+    } else if (turnos == 6 && e !== jugadorActual)
+      document.querySelector(`#casilla${i}`).classList.add("ocupada");
   });
 };
 
@@ -28,27 +27,41 @@ const crearTablero = () => {
 
 const reset = () => {
   tablero = ["", "", "", "", "", "", "", "", ""];
-  tableroLleno = false;
+  turnos = 0;
   ganador.classList.remove("jugadorGana");
-  ganador.classList.remove("iaGana");
   ganador.innerText = "";
   crearTablero();
 };
 
-// jugar pvp
+// jugar pvp (duda sobre argumentos)
 
 const mover = (i) => {
-  if (tablero[i] == "") {
+  if (turnos < 6 && tablero[i] == "") {
     tablero[i] = jugadorActual;
-    crearTablero();
+
     comprobarGanador();
     cambiarTurno();
+    crearTablero();
+  } else if (turnos===6) quitar(i)
+};
+
+const quitar = (i) => {
+  if (tablero[i] === jugadorActual) {
+    tablero[i] = "";
+    turnos--;
+    crearTablero()
+    return null
   }
 };
 
+// comprobar si hay un ganador
+
+const comprobarGanador = () => {};
+
 // cambiar turno
 const cambiarTurno = () => {
-  jugadorActual = jugadorActual === jugador1 ? jugador2 : jugador1
-}
+  jugadorActual = jugadorActual === jugador1 ? jugador2 : jugador1;
+  turnos++;
+};
 
 crearTablero();
